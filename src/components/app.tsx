983493abd0245ab952df45ef59aec592d0e36a3b
Jeremy <jeremy.k.coleman@outlook.com>
2018-12-09 17:49:49 -0500

    Update app.tsx

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -11,10 +11,6 @@ import {observer, inject} from 'mobx-react'
 import messageStore from '../stores/MessageStore'
 import {MessageStore} from '../stores/MessageStore'
 
-let stores = {
-  messages: new MessageStore()
-}
-
 
 type Props = {
   messages?: MessageStore
@@ -43,6 +39,15 @@ export class App extends React.Component<Props, any>  {
   }
 }
 
+//you can also do 
+// let stores = {
+//   messages: new MessageStore()
+// }
+// <Provider {...stores}/>
+// if you havn't used mobx before, this is pretty crucial to understand for using @inject
+// the @inject decorator or inject() hoc uses the name directly provided to the provider component
+// or the keys of the spread object you provide. IE: in the above case using store ={messages: new MessageStore()}
+// the injection key will be 'messages'
 
 @observer
 export class AppContainer extends React.Component<any, any>  {
